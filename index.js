ee136883c88b73ddfd46ddfedba348ca1e3e6b6d
becem <99251251+becem-gharbi@users.noreply.github.com>
2022-12-16 17:32:17 +0100

    v1.0.0

---
+++ index.js
@@ -0,0 +1,47 @@
+const nodemailer = require("nodemailer");
+const { OAuth2Client } = require("google-auth-library");
+
+module.exports = {
+  init: ({ clientId, clientSecret, refreshToken, clientEmail }) => {
+    var transport = null;
+
+    const oAuth2Client = new OAuth2Client(
+      clientId,
+      clientSecret,
+      "https://developers.google.com/oauthplayground"
+    );
+
+    oAuth2Client.setCredentials({ refresh_token: refreshToken });
+
+    oAuth2Client.getAccessToken().then((accessToken) => {
+      transport = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: clientEmail,
+          clientId: clientId,
+          clientSecret: clientSecret,
+          refreshToken: refreshToken,
+          accessToken: accessToken,
+        },
+      });
+
+      console.log("******* Gmail initialized *******");
+    });
+
+    return {
+      send: (options) => {
+        return new Promise((resolve, reject) => {
+          if (transport === null) {
+            reject("Gmail provider not initialized");
+          } else {
+            transport
+              .sendMail(options)
+              .then(() => resolve())
+              .catch((error) => reject(error));
+          }
+        });
+      },
+    };
+  },
+};
